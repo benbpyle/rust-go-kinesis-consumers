d1a9a4823f89cd4d9ae157891ea793fb5b297451
Benjamen Pyle <benbpyle@gmail.com>
2024-06-11 09:25:09 -0500

    update dead code

--- subscriber/kinesis-consumer-model-one-go/model.go
+++ subscriber/kinesis-consumer-model-one-go/model.go
@@ -11,17 +11,3 @@ type ModelOne struct {
 	Name      string    `json:"name"`
 	Locations []string  `json:"locations"`
 }
-
-// func (m *ModelOne) UnmarshalJSON(data []byte) error {
-// 	var v ModelOne
-// 	if err := json.Unmarshal(data, &v); err != nil {
-// 		return err
-// 	}
-//
-// 	m.Id = v.Id
-// 	m.Name = v.Name
-// 	m.Locations = v.Locations
-// 	m.WriteTime = v.WriteTime
-// 	m.ReadTime = time.Now()
-// 	return nil
-// }
